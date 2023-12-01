b0606d97a19afc35210663b1e1668d60df4326a4
Doug Hilton <astro-doug-hilton@gmail.com>
2023-12-01 13:11:45 -0500

    Part 1 complete and submitted

--- 2023/day-01/day01.py
+++ 2023/day-01/day01.py
@@ -13,9 +13,20 @@ for input_line in input_file.readlines():
             value=int(input_char)
             if value_count == 1:
                 line_calib_value = value * 10
-            print(line_calib_value)
+            # print(line_calib_value)
 
-        total_calib_value += line_calib_value
+        # total_calib_value += line_calib_value
+    # reverse the input_line and grab the last number (will be the first after reversing the string
+    value_count=0
+    reversed_line = format(input_line[::-1])
+    for rev_input_char in reversed_line:
+        if rev_input_char.isdigit():
+            value_count += 1
+            value=int(rev_input_char)
+            if value_count == 1:
+                line_calib_value += value
+                print(input_line + "----" + str(line_calib_value))
 
+    total_calib_value += line_calib_value
 print("Calibration Value: " + str(total_calib_value))
 input_file.close()
