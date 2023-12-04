20f00dd2d747ca1aae81f3db7bdf7f2596268145
Doug Hilton <astro-doug-hilton@gmail.com>
2023-12-04 13:47:37 -0500

    Day 02 - Part II complete

--- 2023/day-02/day02.py
+++ 2023/day-02/day02.py
@@ -39,3 +39,48 @@ for input_line in input_file.readlines():
     print(str(sum_possible_games))
 input_file.close()
 print("Sum of possible games: " + str(sum_possible_games))
+
+print("Puzzle 02 Part II - Cube Conundrum")
+print("Loading input...")
+input_file = open("input.txt", "r")
+
+sum_powers = 0
+
+for input_line in input_file.readlines():
+    game_power = 0
+    game_num = 0
+    max_red = 0
+    max_green = 0
+    max_blue = 0
+
+    line_split = input_line.split(":")
+    game_num = int(line_split[0].replace("Game ", ""))
+    print(input_line)
+    # print(str(game_num))
+    games_split = line_split[1].strip().split(";")
+    for game in games_split:
+        each_game_dice = game.strip().split(",")
+        # print(each_game_dice)
+        for game_die in each_game_dice:
+            result = game_die.strip().split(" ")
+            # print (result)
+            match result[1]:
+                case "red":
+                    if int(result[0]) > max_red:
+                        max_red = int(result[0])
+                case "green":
+                    if int(result[0]) > max_green:
+                        max_green = int(result[0])
+                case "blue":
+                    if int(result[0]) > max_blue:
+                        max_blue = int(result[0])
+                case _:
+                    print("Unknown die: " + result[1] + "with value of" + result[0])
+
+    game_power = max_red * max_green * max_blue
+    print("Game Power: " + str(game_power))
+    sum_powers += game_power
+
+    print(str(sum_powers))
+input_file.close()
+print("Sum of powers: " + str(sum_powers))
