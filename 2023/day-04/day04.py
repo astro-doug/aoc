87205ccc66423ad97316368313f09672136e9c85
Doug Hilton <astro-doug-hilton@gmail.com>
2023-12-07 17:58:09 -0500

    Part II for Day04 complete

--- 2023/day-04/day04.py
+++ 2023/day-04/day04.py
@@ -16,10 +16,16 @@ def main():
     logging.debug("Loading input...")
 
     total_points = 0
+    lottery_cards = []
+    # new_lottery_cards = []
 
     with open("input.txt", "r") as input_file:
         for input_line in input_file.readlines():
             lottery_card = input_line.replace(":", "|").split("|")
+            lottery_card.append(1)
+            # store away for part II so I don't have to re-read the file
+            lottery_cards.append(lottery_card)
+
             logging.debug(lottery_card)
             card_number = lottery_card[0]
             logging.debug(card_number)
@@ -36,13 +42,48 @@ def main():
             for num_on_card in card_numbers:
                 if num_on_card in winning_numbers:
                     count_matches += 1
-            logging.debug(f"Card Matches: {count_matches}")
+            logging.info(f"Count of Number Matches: {count_matches}")
             if count_matches > 0:
                 total_points += 2**(count_matches-1)
-            logging.info(f"Total Points: {total_points}")
+            logging.info(f"Sub-Total Points: {total_points}")
 
     logging.debug("Done")
     logging.info(f"Total Points: {total_points}")
+
+    logging.info("Puzzle 04 Part II - Scratchcards")
+    logging.debug("Looping through Lottery Cards Again...")
+    total_cards = 0
+
+    for lottery_card in lottery_cards:
+        logging.debug(lottery_card)
+        card_number = lottery_card[0]
+        logging.debug(card_number)
+
+        winning_numbers = list(map(int, re.split(r'\s+', lottery_card[1].strip())))
+        winning_numbers.sort()
+        logging.debug(f"Winning Numbers: {winning_numbers}")
+
+        card_numbers = list(map(int, re.split(r'\s+', lottery_card[2].strip())))
+        card_numbers.sort()
+        logging.debug(f"Cards Numbers: {card_numbers}")
+        count_matches = 0
+        for num_on_card in card_numbers:
+            if num_on_card in winning_numbers:
+                count_matches += 1
+        logging.debug(f"Count of Number Matches: {count_matches}")
+
+        if count_matches > 0:
+            logging.info(f"Will create {lottery_card[3]} copies of each of the next {count_matches} cards")
+            # cards_to_add = []
+            for counter in range(count_matches):
+                card_to_add = lottery_cards[lottery_cards.index(lottery_card) + counter + 1]
+                card_to_add[3] += lottery_card[3]
+                logging.debug(f"Card: {card_to_add[0]} now has {card_to_add[3]} occurrences")
+
+    for lottery_card in lottery_cards:
+        total_cards += lottery_card[3]
+    logging.info(f"There are {total_cards} lottery cards in the stack")
+
 
 if __name__ == '__main__':
     main()
