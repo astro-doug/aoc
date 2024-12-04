62647ed79140eb8ea6fff7919e99a10bec1c9e74
Doug Hilton <astro-doug-hilton@gmail.com>
2024-12-03 21:37:02 -0500

    2024 - Day02 - Part 2 - Not working yet. Need to think on the algorithm for a bit, but committing what I have

--- 2024/day-02/day02.py
+++ 2024/day-02/day02.py
@@ -13,31 +13,52 @@ def main() -> None:
     logging.info("https://adventofcode.com/2024/day/2")
     logging.info("Puzzle 02 Part I - Red-Nosed Reports")
     reports: list[list[int]] = load_reports()
-    logging.info(f"Reports: {reports}")
+    # logging.info(f"Reports: {reports}")
+    good_reports = check_reports(reports, dampener_engaged=False)
+    logging.info(f"Number of good reports: {good_reports}")
+    logging.info("Puzzle 02 Part II - Red-Nosed Reports")
+    # new_reports: list[list[int]] = load_reports()
+    # good_reports = check_reports(new_reports, dampener_engaged=True)
+    # logging.info(f"Number of good reports: {good_reports}")
+
+
+def check_reports(reports: list[list[int]], dampener_engaged: bool = False) -> int:
     good_reports: int = 0
     for x, report in enumerate(reports):
-        if check_report_safe(report):
+        if check_report_safe(report, False, dampener_engaged):
             good_reports += 1
             logging.info(f"Good report ascending. tally: {good_reports}")
         else:
             report.reverse()
-            if check_report_safe(report):
+            if check_report_safe(report, True, dampener_engaged):
                 good_reports += 1
                 logging.info(f"Good report descending. tally: {good_reports}")
-    logging.info(f"Number of good reports: {good_reports}")
+    return good_reports
 
 
-def check_report_safe(report: list[int]) -> bool:
+# Need to refactor since it's no longer a binary condition because of phase II - one fault can be tolerated
+# going to keep track of faults, and if that count is 0 or 1, then safe = True, else False
+def check_report_safe(report: list[int], backwards: bool = False, dampener_engaged: bool = False) -> bool:
     safe: bool = False
     prev_value: int = 0
+    bad_count: int = 0
 
     for x, value in enumerate(report):
-        safe = True if prev_value == 0 or (prev_value + 3 >= value > prev_value and value != prev_value) else False
-        logging.info(f"{report}: value: {value} -- prev_value: {prev_value} --- Safe: {safe}")
+        #  if not backwards:
+        safe = True if prev_value == 0 or ((prev_value + 3 >= value > prev_value) and value != prev_value) else False
+        # else:
+            # safe = True if prev_value == 0 or ((prev_value - 3 <= value < prev_value) and value != prev_value) else False
+        if not safe:
+            bad_count += 1
+        logging.info(f"{backwards} - {report}: value: {value} -- prev_value: {prev_value} dampener: {dampener_engaged} bad_count: {bad_count} --- Safe: {safe}")
         prev_value = value
         if not safe:
-            break
-    return safe
+            if not dampener_engaged:
+                break
+    if dampener_engaged:
+        return bad_count <= 1
+    else:
+        return safe
 
 
 def load_reports() -> list[list[int]]:
